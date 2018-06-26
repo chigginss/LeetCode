f108c0dff52457786a61db3d7c6fbcb97fafb8e2
Cierra Higgins <cierramhiggins@gmail.com>
2018-06-26 14:14:10 -0700

    added to algorithms

--- algorithms.py
+++ algorithms.py
@@ -7,6 +7,24 @@ You may assume that each input would have exactly one solution, and you may not 
             for j in range(len(nums)):
                 if nums[i] + nums[j] == target and i != j:
                     return [i, j]
+
+""" Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward."""
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
         
+        num = str(x)
+        
+        for i in range(0, len(num)/2):
+            if num[i] != num[len(num)-i-1]:
+                return False
+        
+        return True
+        
+
         
         
